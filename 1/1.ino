4c4ba94c1fd07b15749e4149485f5eb6332f9c5d
asengsaragih <aldiwahyu.saragih@gmail.com>
2019-12-09 09:21:12 +0700

    nomor 1

--- 1/1.ino
+++ 1/1.ino
@@ -1,9 +1,40 @@
+int led3 = 6;
+int led2 = 10;
+int led1 = 11;
+int pushButton = 7;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(pushButton, INPUT);
 
+  digitalWrite(pushButton, HIGH);
+
+//  digitalWrite(led1, HIGH);
+//  digitalWrite(led2, HIGH);
+//  digitalWrite(led3, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if (digitalRead(pushButton) == LOW) {
+    for (int i = 0; i <= 255; i+=5) {
+      analogWrite(led1, i);
+      analogWrite(led2, i);
+      analogWrite(led3, i);
+
+      delay(100);
+    }
+
+    for (int i = 255; i >= 0; i-=5) {
+      analogWrite(led1, i);
+      analogWrite(led2, i);
+      analogWrite(led3, i);
+
+      delay(100);
+    }
+  }
 }
